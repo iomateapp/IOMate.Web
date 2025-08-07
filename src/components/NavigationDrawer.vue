6a0901e70c2886de6b7d00429a666fd017e0345d
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2025-08-06 21:21:43 -0300

    first commit

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    permanent
+    rail
+    class="pa-2"
+  >
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        subtitle="sandra_a88@gmail.com"
+        title="Sandra Adams"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-folder"
+        title="Meus Arquivos"
+        value="myfiles"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account-multiple"
+        title="Compartilhado"
+        value="shared"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-star"
+        title="Favoritos"
+        value="starred"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-layout>
+
+      <!-- Navigation Drawer -->
+      <NavigationDrawer />
+
+      <!-- App Content -->
+      <v-main>
+        <v-container fluid>
+          <v-card>
+            <v-card-title>Conteúdo principal</v-card-title>
+            <v-card-text>
+              O conteúdo se ajusta automaticamente ao tamanho do drawer.
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-main>
+
+    </v-layout>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(true)
+const rail = ref(true)
+</script>
+       
